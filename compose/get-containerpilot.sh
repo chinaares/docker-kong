321e4c7e3997b00129c65028e86c8308a3ad2f07
Ares <yangzibin.cn@hotmail.com>
2018-01-09 14:59:42 +0800

    For my local test envirment modified.

---
+++ compose/get-containerpilot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export https_proxy=http://127.0.0.1:1080/pac?auth=s0kbJ38VJOD2PdPqQUiC\&t=201801080910381508
+
+#CONTAINERPILOT_VERSION=3.6.2
+CONTAINERPILOT_VERSION=2.7.2
+echo Prepare Download https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz ......
+curl -Lso /tmp/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz \
+"https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz"
+curl -Lso /tmp/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt \
+"https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt"
+cat /tmp/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt
+cp -f /tmp/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz kong/containerpilot.tar.gz
+
+
+CONTAINERPILOT_VERSION=2.4.1
+echo Prepare Download https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz ......
+curl -Lso /tmp/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz \
+"https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz"
+curl -Lso /tmp/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt \
+"https://github.com/joyent/containerpilot/releases/download/${CONTAINERPILOT_VERSION}/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt"
+cat /tmp/containerpilot-${CONTAINERPILOT_VERSION}.sha1.txt
+cp -f /tmp/containerpilot-${CONTAINERPILOT_VERSION}.tar.gz nginx/containerpilot.tar.gz
+
+
+
